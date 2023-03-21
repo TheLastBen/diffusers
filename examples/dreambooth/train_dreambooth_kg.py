66122414d6f941911846ee79799af04b415ff8a0
Ben <110583491+TheLastBen@users.noreply.github.com>
2023-03-21 11:32:26 +0100

    Update train_dreambooth_kg.py

--- examples/dreambooth/train_dreambooth_kg.py
+++ examples/dreambooth/train_dreambooth_kg.py
@@ -559,7 +559,7 @@ def main():
 
     if args.scale_lr:
         args.learning_rate = (
-            args.learning_rate * args.gradient_accumulation_steps * args.train_batch_size * accelerator.num_processes
+            args.learning_rate * args.gradient_accumulation_steps * 1 * accelerator.num_processes
         )
 
     # Use 8-bit Adam for lower memory usage or to fine-tune the model in 16GB GPUs
